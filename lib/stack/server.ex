4880ef67c042e6a5f52f6c4c954177b13b8f6b83
Matija Krajnik <matija.krajnik90@gmail.com>
2017-05-15 22:40:57 +0200

    added api for stack server and handling of terminate function

--- lib/stack/server.ex
+++ lib/stack/server.ex
@@ -1,11 +1,39 @@
 defmodule Stack.Server do
   use GenServer
   
+  def start_server(initial_stack) do
+    GenServer.start_link(__MODULE__, initial_stack, name: __MODULE__)
+  end
+  
+  def pop() do
+    GenServer.call(__MODULE__, :pop)
+  end
+  
+  def push(new_number) do
+    GenServer.cast(__MODULE__, {:push, new_number})
+  end
+  
+  def shutdown(reason \\ :shutdown) do
+    GenServer.cast(__MODULE__, {:stop, reason})
+  end
+  
   def handle_call(:pop, _from, [head | tail]) do
     { :reply, head, tail }
   end
   
   def handle_cast({:push, head}, tail) do
-    { :noreply, [head | tail]}
+    { :noreply, [head | tail] }
+  end
+  
+  def handle_cast({:stop, reason}, stack) do
+    { :stop, reason, stack }
+  end
+  
+  def terminate(reason, stack) do
+    IO.puts """
+    Server terminated.
+    Reason: #{reason}
+    Last state: #{inspect stack}
+    """
   end
 end
