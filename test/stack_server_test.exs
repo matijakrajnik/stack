e584dd1f298ad8751ab29024a39766225d08b1c8
Matija Krajnik <matija.krajnik90@gmail.com>
2017-05-19 20:45:42 +0200

    added basic tests for stack server

---
+++ test/stack_server_test.exs
@@ -0,0 +1,32 @@
+defmodule StackServerTest do
+  use ExUnit.Case
+  doctest Stack.Server
+  @initial_state Application.get_env(:stack, :initial_state)
+  @new_element 123
+  
+  import Stack.Server, only: [pop: 0, push: 1]
+  
+  setup do
+    :sys.replace_state(Stack.Server, fn {_old_state, pid} -> {@initial_state, pid} end)
+    :ok
+  end
+  
+  test "server started with initial state" do
+    {server_state, pid} = :sys.get_state(Stack.Server)
+    assert server_state == @initial_state
+    assert pid != nil
+  end
+  
+  test "pop from stack server" do
+    {poped_element, new_list} = List.pop_at(@initial_state, 0)
+    assert pop() == poped_element
+    {poped_element, _} = List.pop_at(new_list, 0)
+    assert pop() == poped_element
+  end
+  
+  test "push to stack server" do
+    assert push(@new_element) == :ok
+    {server_state, _pid} = :sys.get_state(Stack.Server)
+    assert server_state == [@new_element | @initial_state]
+  end
+end
